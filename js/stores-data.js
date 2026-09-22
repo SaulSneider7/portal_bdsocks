39035385ccb149b4576c73b4f614520330da5738
Saul Sneider <saulsneider@hotmail.com>
2026-09-22 17:18:08 -0500

    my first commit

---
+++ js/stores-data.js
@@ -0,0 +1,140 @@
+/**
+ * Datos oficiales de puntos de venta de BD SOCKS
+ */
+const STORES_DATA = [
+  {
+    id: "barbadens",
+    name: "Tienda Barbadens",
+    logo: "img/stores/barbadens-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "Ca. Chinchón 944, San Isidro",
+        district: "San Isidro",
+        mapUrl: "https://maps.app.goo.gl/TQRsNu17YyJbUCBB8"
+      },
+      {
+        address: "Circunvalación Golf Los Incas 170, Surco",
+        district: "Santiago de Surco",
+        mapUrl: "https://maps.google.com/?q=Av.+Club+Golf+los+Incas+170,+Santiago+de+Surco+15023"
+      }
+    ]
+  },
+  {
+    id: "japy",
+    name: "Tienda Japy Detalles y Globos",
+    logo: "img/stores/japy-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "Camino Real 485, San Isidro",
+        district: "San Isidro",
+        mapUrl: "https://maps.app.goo.gl/LUpS6qctswgSNBJ1A"
+      }
+    ]
+  },
+  {
+    id: "anel",
+    name: "Tienda Anel Joyería",
+    logo: "img/stores/anel-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "Pje. Porta 115, Tienda 1, Miraflores",
+        district: "Miraflores",
+        mapUrl: "https://maps.app.goo.gl/oz7jmT8UyBP5yULj8"
+      }
+    ]
+  },
+  {
+    id: "ojos-de-luna",
+    name: "Tienda Ojos de Luna",
+    logo: "img/stores/ojos-de-luna-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "CC La Rotonda 2050, La Molina",
+        district: "La Molina",
+        mapUrl: "https://maps.app.goo.gl/TGFtocgerTMcnvmp8"
+      }
+    ]
+  },
+  {
+    id: "4essen",
+    name: "Tienda 4Essen",
+    logo: "img/stores/4essen-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "Av. Conquistadores 489, San Isidro",
+        district: "San Isidro",
+        mapUrl: "https://maps.app.goo.gl/CyF9yoCXEJcMnxYh9"
+      }
+    ]
+  },
+  {
+    id: "amavia",
+    name: "Tienda Amavia",
+    logo: "img/stores/amavia-logo.webp",
+    city: "Trujillo",
+    locations: [
+      {
+        address: "Av. Juan Pablo II 417 (frente a la 2da puerta UNT)",
+        district: "Trujillo",
+        mapUrl: "https://maps.app.goo.gl/mr8RUrBJngbjBFKi9"
+      }
+    ]
+  },
+  {
+    id: "malena",
+    name: "Tienda Malena Concept",
+    logo: "img/stores/malena-logo.webp",
+    city: "Trujillo",
+    locations: [
+      {
+        address: "Calle La Arboleda 771, California",
+        district: "Trujillo",
+        mapUrl: "https://maps.app.goo.gl/GqCxL6RqVzXAR9hQ8"
+      }
+    ]
+  },
+  {
+    id: "cocodeli",
+    name: "CocoDeli Concept",
+    logo: "img/stores/cocodeli-logo.webp",
+    city: "Ayacucho",
+    locations: [
+      {
+        address: "Jr. Cusco 202, Ayacucho",
+        district: "Ayacucho",
+        mapUrl: "https://maps.app.goo.gl/71HKg4cdUx53s5vC7"
+      }
+    ]
+  },
+  {
+    id: "lollipops",
+    name: "LOLLIPOPS",
+    logo: "img/stores/lollipops-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "Av. Larco 345, Tda M2, Miraflores",
+        district: "Miraflores",
+        mapUrl: "https://maps.app.goo.gl/wiT87LnTRQeoUyeh7"
+      }
+    ]
+  },
+  {
+    id: "magna",
+    name: "MAGNA",
+    logo: "img/stores/magna-logo.webp",
+    city: "Lima",
+    locations: [
+      {
+        address: "C.C. Caminos del Inca, Tda. 75 2do piso, Surco",
+        district: "Santiago de Surco",
+        mapUrl: "https://maps.app.goo.gl/pr2ibx84CCMp8RrcA"
+      }
+    ]
+  }
+];
